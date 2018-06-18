280857b1b82139d052e6af8710e86a04c6c4a6d6
Ethan Bellem <ethan.bellem@ocsbstudent.ca>
2018-06-18 13:25:18 -0400

    update

--- scene/game/lib/jackOLanternEnemy.lua
+++ scene/game/lib/jackOLanternEnemy.lua
@@ -8,18 +8,18 @@ local composer = require( "composer" )
 
 function M.new( instance )
 
-    if not instance then error( "ERROR: Expected display object" ) end
+	if not instance then error( "ERROR: Expected display object" ) end
 
-    -- Get scene and sounds
-    local scene = composer.getScene( composer.getSceneName( "current" ) )
-    local sounds = scene.sounds
+	-- Get scene and sounds
+	local scene = composer.getScene( composer.getSceneName( "current" ) )
+	local sounds = scene.sounds
 
-    -- Store map placement and hide placeholder
-    instance.isVisible = false
-    local parent = instance.parent
-    local x, y = instance.x, instance.y
+	-- Store map placement and hide placeholder
+	instance.isVisible = false
+	local parent = instance.parent
+	local x, y = instance.x, instance.y
 
-    -- Load spritesheet
+	-- Load spritesheet
 
     -- our character
     local sheetOptionsIdlejack = require("assets.spritesheets.jack.jackIdle")
@@ -50,97 +50,97 @@ function M.new( instance )
     }
 
     instance = display.newSprite( parent, sheetIdlejack, sequence_data )
-    instance.x,instance.y = x, y
-    instance:setSequence( "walk" )
-    instance:play()
+	instance.x,instance.y = x, y
+	instance:setSequence( "walk" )
+	instance:play()
 
 
-    -- Load spritesheet
-    --local sheetData = { width = 192, height = 256, numFrames = 79, sheetContentWidth = 1920, sheetContentHeight = 2048 }
-    --local sheet = graphics.newImageSheet( "scene/game/img/sprites.png", sheetData )
-    --local sequenceData = {
-    --    { name = "idle", frames = { 21 } },
-    --    { name = "walk", frames = { 22, 23, 24, 25 } , time = 500, loopCount = 0 },
-    --}
-    --instance = display.newSprite( parent, sheet, sequenceData )
-    --instance.x, instance.y = x, y
-    --instance:setSequence( "walk" )
-    --instance:play()
+	-- Load spritesheet
+	--local sheetData = { width = 192, height = 256, numFrames = 79, sheetContentWidth = 1920, sheetContentHeight = 2048 }
+	--local sheet = graphics.newImageSheet( "scene/game/img/sprites.png", sheetData )
+	--local sequenceData = {
+	--	{ name = "idle", frames = { 21 } },
+	--	{ name = "walk", frames = { 22, 23, 24, 25 } , time = 500, loopCount = 0 },
+	--}
+	--instance = display.newSprite( parent, sheet, sequenceData )
+	--instance.x, instance.y = x, y
+	--instance:setSequence( "walk" )
+	--instance:play()
 
-    -- Add physics
-    physics.addBody( instance, "dynamic", { radius = 100, density = 3, bounce = 0, friction =  1.0 } )
-    instance.isFixedRotation = true
-    instance.anchorY = 0.77
-    instance.angularDamping = 3
-    instance.isDead = false
+	-- Add physics
+	physics.addBody( instance, "dynamic", { radius = 100, density = 3, bounce = 0, friction =  1.0 } )
+	instance.isFixedRotation = true
+	instance.anchorY = 0.77
+	instance.angularDamping = 3
+	instance.isDead = false
 
-    function instance:die()
-        audio.play( sounds.sword )
-        self.isFixedRotation = false
-        self.isSensor = true
-        self:applyLinearImpulse( 0, -200 )
-        self.isDead = true
-    end
+	function instance:die()
+		audio.play( sounds.sword )
+		self.isFixedRotation = false
+		self.isSensor = true
+		self:applyLinearImpulse( 0, -200 )
+		self.isDead = true
+	end
 
-    function instance:preCollision( event )
-        local other = event.other
-        local y1, y2 = self.y + 50, other.y - other.height/2
-        -- Also skip bumping into floating platforms
-        if event.contact and ( y1 > y2 ) then
-            if other.floating then
-                event.contact.isEnabled = false
-            else
-                event.contact.friction = 0.1
-            end
-        end
-    end
+	function instance:preCollision( event )
+		local other = event.other
+		local y1, y2 = self.y + 50, other.y - other.height/2
+		-- Also skip bumping into floating platforms
+		if event.contact and ( y1 > y2 ) then
+			if other.floating then
+				event.contact.isEnabled = false
+			else
+				event.contact.friction = 0.1
+			end
+		end
+	end
 
-    local max, direction, flip, timeout = 250, 5000, 0.133, 0
-    direction = direction * ( ( instance.xScale < 0 ) and 1 or -1 )
-    flip = flip * ( ( instance.xScale < 0 ) and 1 or -1 )
+	local max, direction, flip, timeout = 250, 5000, 0.133, 0
+	direction = direction * ( ( instance.xScale < 0 ) and 1 or -1 )
+	flip = flip * ( ( instance.xScale < 0 ) and 1 or -1 )
 
-    local function enterFrame()
+	local function enterFrame()
 
-        -- Do this every frame
-        local vx, vy = instance:getLinearVelocity()
-        local dx = direction
-        if instance.jumping then dx = dx / 4 end
-        if ( dx < 0 and vx > -max ) or ( dx > 0 and vx < max ) then
-            instance:applyForce( dx or 0, 0, instance.x, instance.y )
-        end
-        
-        -- Bumped
-        if math.abs( vx ) < 1 then
-            timeout = timeout + 1
-            if timeout > 30 then
-                timeout = 0
-                direction, flip = -direction, -flip
-            end
-        end
+		-- Do this every frame
+		local vx, vy = instance:getLinearVelocity()
+		local dx = direction
+		if instance.jumping then dx = dx / 4 end
+		if ( dx < 0 and vx > -max ) or ( dx > 0 and vx < max ) then
+			instance:applyForce( dx or 0, 0, instance.x, instance.y )
+		end
+		
+		-- Bumped
+		if math.abs( vx ) < 1 then
+			timeout = timeout + 1
+			if timeout > 30 then
+				timeout = 0
+				direction, flip = -direction, -flip
+			end
+		end
 
-        -- Turn around
-        instance.xScale = math.min( 1, math.max( instance.xScale + flip, -1 ) )
-    end
+		-- Turn around
+		instance.xScale = math.min( 1, math.max( instance.xScale + flip, -1 ) )
+	end
 
-    function instance:finalize()
-        -- On remove, cleanup instance, or call directly for non-visual
-        Runtime:removeEventListener( "enterFrame", enterFrame )
-        instance = nil
-    end
+	function instance:finalize()
+		-- On remove, cleanup instance, or call directly for non-visual
+		Runtime:removeEventListener( "enterFrame", enterFrame )
+		instance = nil
+	end
 
-    -- Add a finalize listener (for display objects only, comment out for non-visual)
-    instance:addEventListener( "finalize" )
+	-- Add a finalize listener (for display objects only, comment out for non-visual)
+	instance:addEventListener( "finalize" )
 
-    -- Add our enterFrame listener
-    Runtime:addEventListener( "enterFrame", enterFrame )
+	-- Add our enterFrame listener
+	Runtime:addEventListener( "enterFrame", enterFrame )
 
-    -- Add our collision listener
-    instance:addEventListener( "preCollision" )
+	-- Add our collision listener
+	instance:addEventListener( "preCollision" )
 
-    -- Return instance
-    instance.name = "enemy"
-    instance.type = "enemy"
-    return instance
+	-- Return instance
+	instance.name = "enemy"
+	instance.type = "enemy"
+	return instance
 end
 
 return M
